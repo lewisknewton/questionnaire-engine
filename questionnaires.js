f4557e46b6d964f82f473c74198b0d0c7848d966
Lewis Newton <lewisknewton@hotmail.com>
2020-02-28 16:45:29 +0000

    read questionnaire files stored in local directory
    - needs to read from database too

--- questionnaires.js
+++ questionnaires.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const { Client } = require('pg');
 const config = require('./config');
 
+const localDir = './public/questionnaires';
 const dbClient = new Client(config);
 
 dbClient.connect();
@@ -12,13 +15,48 @@ dbClient.on('error', (err) => {
   dbClient.end();
 });
 
+async function getStats(dirPath) {
+  const itemStats = {};
+  const items = await fs.promises.readdir(dirPath);
+
+  for (const item of items) {
+    const filePath = path.join(dirPath, item);
+    const stats = await fs.promises.stat(filePath);
+
+    itemStats[item] = { path: filePath, isFile: stats.isFile() };
+  }
+
+  return itemStats;
+}
+
 async function selectQuestionnaires() {
+  const questionnaires = {};
+
+  try {
+    const itemStats = await getStats(localDir);
+
+    // Add questionnaires stored in local directory
+    for (const name in itemStats) {
+      if (itemStats[name].isFile) {
+        const file = await fs.promises.readFile(itemStats[name].path);
+
+        questionnaires[name] = JSON.parse(file);
+        questionnaires[name].local = true;
+      }
+    }
+
+    // Add questionnaires stored in database
   const query = 'SELECT * FROM questionnaire';
   const result = await dbClient.query(query);
 
-  if (result.rows.length > 0) {
-    return result.rows;
+    // if (result.rows.length > 0) 
+    questionnaires.test = 'None in database';
+
+    return questionnaires;
+  } catch (e) {
+    console.error(e);
   }
+}
 
   return 'Sorry, no questionnaires were found.';
 }
