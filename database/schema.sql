7bc3d27120689068cac6c8023a0bac2b52d8e8fb
Lewis Newton <lewisknewton@hotmail.com>
2020-05-05 16:28:10 +0100

    clean up schema

--- database/schema.sql
+++ database/schema.sql
@@ -3,84 +3,40 @@ DROP DATABASE IF EXISTS questionnaire_engine;
 CREATE DATABASE questionnaire_engine;
 \c questionnaire_engine;
 
-DROP TABLE IF EXISTS user_account;
 DROP TABLE IF EXISTS questionnaire;
-DROP TABLE IF EXISTS taking;
-DROP TABLE IF EXISTS section;
-DROP TABLE IF EXISTS question_type;
 DROP TABLE IF EXISTS question;
 DROP TABLE IF EXISTS response;
-DROP TABLE IF EXISTS option_type;
-DROP TABLE IF EXISTS question_option;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS user_account (
-  id INT,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE IF NOT EXISTS questionnaire (
   id uuid DEFAULT uuid_generate_v4(),
-  title VARCHAR (70),
-  code VARCHAR (20),
-  scored BOOLEAN,
+  name VARCHAR(255) NOT NULL,
+  scored BOOLEAN DEFAULT FALSE,
   file_path TEXT,
-  creator_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (creator_id) REFERENCES user_account (id)
-);
-
-CREATE TABLE IF NOT EXISTS taking (
-  id INT,
-  participant_id INT,
-  questionnaire_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (participant_id) REFERENCES user_account (id),
-  FOREIGN KEY (questionnaire_id) REFERENCES questionnaire (id)
-);
-
-CREATE TABLE IF NOT EXISTS section (
-  id INT,
-  questionnaire_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (questionnaire_id) REFERENCES questionnaire (id)
+  author_email VARCHAR(255),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS question_type (
   id INT,
-  type_description VARCHAR (70),
+  name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS question (
-  id INT,
-  section_id INT,
-  type_id INT,
+  id uuid DEFAULT uuid_generate_v4(),
+  readable_id VARCHAR(30) NOT NULL,
+  text TEXT,
+  questionnaire_id uuid,
   PRIMARY KEY (id),
-  FOREIGN KEY (section_id) REFERENCES section (id),
-  FOREIGN KEY (type_id) REFERENCES question_type (id)
+  FOREIGN KEY (questionnaire_id) REFERENCES questionnaire (id)
 );
 
 CREATE TABLE IF NOT EXISTS response (
   id uuid DEFAULT uuid_generate_v4(),
   time_submitted TIMESTAMP DEFAULT NOW(),
-  questionnaire VARCHAR(255),
+  questionnaire_id VARCHAR(36),
   data JSONB,
   PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS option_type (
-  id INT,
-  type_description VARCHAR (70),
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS question_option (
-  id INT,
-  question_id INT,
-  type_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (question_id) REFERENCES question (id),
-  FOREIGN KEY (type_id) REFERENCES option_type (id)
 );
