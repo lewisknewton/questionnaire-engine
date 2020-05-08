b06c2769ab1130e1f7ed31b4f8ac1e9930bcfc98
Lewis Newton <lewisknewton@hotmail.com>
2020-05-08 14:40:42 +0100

    add unique and friendly IDs
    - separates private primary key and more human-friendly IDs for sharing

--- database/schema.sql
+++ database/schema.sql
@@ -10,41 +10,45 @@ DROP TABLE IF EXISTS response;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS questionnaire (
-  id uuid DEFAULT uuid_generate_v4(),
+  unique_id uuid DEFAULT uuid_generate_v4(),
+  id VARCHAR(8),
   name VARCHAR(255) NOT NULL,
   scored BOOLEAN DEFAULT FALSE,
   file_path TEXT,
   author_email VARCHAR(255),
-  PRIMARY KEY (id)
+  PRIMARY KEY (unique_id)
 );
 
 CREATE TABLE IF NOT EXISTS question_type (
-  id INT,
+  id SERIAL,
   name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS question (
-  id uuid DEFAULT uuid_generate_v4(),
-  readable_id VARCHAR(30) NOT NULL,
+  unique_id uuid DEFAULT uuid_generate_v4(),
+  id VARCHAR(30) NOT NULL,
   text TEXT,
   questionnaire_id uuid,
-  PRIMARY KEY (id),
-  FOREIGN KEY (questionnaire_id) REFERENCES questionnaire (id)
+  PRIMARY KEY (unique_id),
+  FOREIGN KEY (questionnaire_id) REFERENCES questionnaire (unique_id)
 );
 
 CREATE TABLE IF NOT EXISTS question_option (
-  id uuid DEFAULT uuid_generate_v4(),
+  unique_id uuid DEFAULT uuid_generate_v4(),
+  id VARCHAR(8),
   text TEXT,
   question_id uuid,
-  PRIMARY KEY (id),
-  FOREIGN KEY (question_id) REFERENCES question (id)
+  PRIMARY KEY (unique_id),
+  FOREIGN KEY (question_id) REFERENCES question (unique_id)
 );
 
 CREATE TABLE IF NOT EXISTS response (
-  id uuid DEFAULT uuid_generate_v4(),
+  unique_id uuid DEFAULT uuid_generate_v4(),
+  id VARCHAR(8),
   time_submitted TIMESTAMP DEFAULT NOW(),
-  questionnaire_id VARCHAR(36),
+  questionnaire_id uuid,
   data JSONB,
-  PRIMARY KEY (id)
+  PRIMARY KEY (unique_id),
+  FOREIGN KEY (questionnaire_id) REFERENCES questionnaire (unique_id)
 );
