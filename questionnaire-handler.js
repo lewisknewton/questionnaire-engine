345e238c4c0877a0854c40a704faf53c3f192a8f
Lewis Newton <lewisknewton@hotmail.com>
2020-05-19 20:09:39 +0100

    return bool and diffs from comparisons

--- questionnaire-handler.js
+++ questionnaire-handler.js
@@ -35,71 +35,128 @@ async function getStats(dirPath) {
 async function selectDBCopy(path) {
   try {
     const query = `
-      SELECT id,
-             unique_id AS "uniqueId", 
-             name, 
-             scored, 
-             file_path AS path
-      FROM   questionnaire 
-      WHERE  file_path = $1
+      SELECT  questionnaire.unique_id AS "uniqueId",
+              questionnaire.name,
+              questionnaire.scored,
+              questionnaire.file_path AS path,
+              COALESCE(
+                JSON_AGG(questions.*) 
+                FILTER (
+                  WHERE questions."uniqueId" IS NOT NULL
+                ),
+              '[]'
+              ) AS questions
+      FROM (
+        SELECT  question.unique_id AS "uniqueId",
+                question.id,
+                question.text,
+                question.type,
+                question.questionnaire_id,
+                COALESCE(
+                  JSON_AGG(
+                    JSON_BUILD_OBJECT(
+                      'id', question_option.id,
+                      'text', question_option.text
+                    )
+                  ) FILTER (
+                      WHERE question_option.unique_id IS NOT NULL
+                  ),
+                '[]'
+                ) AS options
+        FROM question
+        LEFT JOIN question_option ON question.unique_id = question_option.question_id
+        GROUP BY question.unique_id
+      ) AS questions
+      RIGHT JOIN questionnaire ON questions.questionnaire_id = questionnaire.unique_id
+      WHERE questionnaire.file_path = $1
+      GROUP BY questionnaire.unique_id
     `;
-    const result = await dbClient.query(query, [path]);
 
-    // Return the selected questionnaire
-    return result.rows[0];
+    const result = await dbClient.query(query, [path]);
+    const selected = result.rows[0];
+
+    return selected;
   } catch (err) {
     console.error(err);
   }
 }
 
 /**
- * Compares options for single and multi-select questions in a questionnaire
- * file and their database copies to check if they are the same.
+ * Compares two sets of options for single and multi-select questions to
+ * check if they are the same.
  */
-function checkOptionsUpToDate(originals, copies) {
-  let same = true;
+function compareOptions(originals, copies) {
+  let optionsSame = true;
+  const optionsDiffs = [];
 
   // Helper function to sort options alphabetically
-  const alphabetise = (a, b) => (a > b) ? 1 : -1;
+  const alphabetise = (a, b) => (a.text > b.text) ? 1 : -1;
+
+  // Helper function to copy mismatching options
+  const recordDiff = (first, second) => {
+    const differing = { first, second };
+
+    if (!optionsDiffs.includes(differing)) optionsDiffs.push(differing);
+    optionsSame = false;
+  };
+
+  // Helper function to add missing properties for comparisons
+  const formatOptions = (options) => {
+    for (let i = 0; i < options.length; i += 1) {
+      if (!Object.prototype.hasOwnProperty.call(options[i], 'text')) {
+        options[i] = { text: options[i] };
+      }
+    }
+
+    options.sort(alphabetise);
+  };
 
   if ((!filled(originals) !== !filled(copies)) || !sameLength(originals, copies)) {
     // If one set is filled, one set empty, or both have different lengths
-    same = false;
+    optionsSame = false;
   } else if (filled(originals) && filled(copies)) {
-    originals.sort(alphabetise);
-    copies.sort(alphabetise);
+    for (const arr of [originals, copies]) formatOptions(arr);
 
     for (let i = 0; i < originals.length; i += 1) {
-      if (originals[i] !== copies[i]) same = false;
+      if (originals[i].text !== copies[i].text) recordDiff(originals[i], copies[i]);
     }
   }
 
-  return same;
+  return { optionsSame, optionsDiffs };
 }
 
 /**
- * Compares questions in a questionnaire file and their database copies to
- * check if they are the same.
+ * Compares two sets of questions to check if they are the same.
  */
-function checkQuestionsUpToDate(originals, copies) {
-  let same = true;
+function compareQuestions(originals, copies) {
+  let questionsSame = true;
+  const questionsDiffs = [];
 
-  // Question-level properties to compare
   const toCompare = ['id', 'text', 'type'];
 
   // Helper function to sort questions by ID alphabetically
   const alphabetise = (a, b) => (a.id > b.id) ? 1 : -1;
 
-  if ((!filled(originals) !== !filled(copies)) || sameLength(originals, copies)) {
+  // Helper function to copy mismatching questions
+  const recordDiff = (first, second) => {
+    const differing = { first, second };
+
+    if (questionsDiffs[questionsDiffs.length] === differing) console.log('ye');
+    // if (questionsDiffs.includes(differing)) console.log('ye');
+
+    if (!questionsDiffs.includes(differing)) questionsDiffs.push(differing);
+    questionsSame = false;
+  };
+
+  if ((!filled(originals) !== !filled(copies)) || !sameLength(originals, copies)) {
     // If one set is filled, one set empty, or both have different lengths
-    same = false;
+    questionsSame = false;
   } else if (filled(originals) && filled(copies)) {
-    originals.sort(alphabetise);
-    copies.sort(alphabetise);
+    for (const arr of [originals, copies]) arr.sort(alphabetise);
 
     for (let i = 0; i < originals.length; i += 1) {
       for (const prop of toCompare) {
-        if (originals[i][prop] !== copies[i][prop]) same = false;
+        if (originals[i][prop] !== copies[i][prop]) recordDiff(originals[i], copies[i]);
       }
 
       const originalOptions = originals[i].options;
@@ -107,27 +164,37 @@ function checkQuestionsUpToDate(originals, copies) {
 
       // Check options if there are any (single or multi-select questions)
       if (filled(originalOptions) || filled(copyOptions)) {
-        same = same && checkOptionsUpToDate(originalOptions, copyOptions);
+        const { optionsSame } = compareOptions(originalOptions, copyOptions);
+
+        if (!optionsSame) recordDiff(originals[i], copies[i]);
       }
     }
   }
 
-  return same;
+  return { questionsSame, questionsDiffs };
 }
 
 /**
- * Compares a questionnaire file and its database copy to check if they are the same.
+ * Compares two questionnaires to check if they are the same.
  */
-function checkUpToDate(original, copy) {
+function compareQuestionnaires(original, copy) {
   let same = true;
+  const diffs = [];
 
-  // Top-level properties to compare
   const toCompare = ['name', 'scored', 'path', 'author_email'];
+
+  // Helper function to copy mismatching questionnaires
+  const recordDiff = (first, second) => {
+    const differing = { first, second };
+
+    if (!diffs.includes(differing)) diffs.push(differing);
+    same = false;
+  };
 
   // Compare properties, ignoring optional booleans (e.g. scored)
   for (const prop of toCompare) {
     if (original[prop] == null && [null, false].includes(copy[prop])) continue;
-    if (original[prop] !== copy[prop]) same = false;
+    if (original[prop] !== copy[prop]) recordDiff(original, copy);
   }
 
   const originalQuestions = original.questions;
@@ -135,10 +202,12 @@ function checkUpToDate(original, copy) {
 
   // Check questions if there are any
   if (filled(originalQuestions) || filled(copyQuestions)) {
-    same = same && checkQuestionsUpToDate(originalQuestions, copyQuestions);
+    const { questionsSame } = compareQuestions(originalQuestions, copyQuestions);
+
+    if (!questionsSame) recordDiff(original, copy);
   }
 
-  return same;
+  return { same, diffs };
 }
 
 /**
@@ -170,7 +239,8 @@ async function selectQuestionnaire(name, dir = localDir) {
 }
 
 /**
- * Synchronises all file-based questionnaires in a directory with their database records.
+ * Synchronises all file-based questionnaires in the directory with their
+ * database records.
  */
 async function syncQuestionnaires(dir) {
   try {
@@ -187,8 +257,10 @@ async function syncQuestionnaires(dir) {
         // Add to the database if not already stored there
         if (copy == null) {
           await addQuestionnaire(questionnaire);
-        } else if (!checkUpToDate(questionnaire, copy)) {
-          await updateQuestionnaire(copy.uniqueId, questionnaire);
+        } else {
+          const { same } = compareQuestionnaires(questionnaire, copy);
+
+          if (!same) await updateQuestionnaire(copy.uniqueId, questionnaire);
         }
       } else {
         // If a directory is found, look for questionnaires inside it
@@ -228,9 +300,18 @@ async function selectQuestionnaires() {
 async function addOption(questionId, option) {
   try {
     const query = `
-      INSERT INTO question_option (text, question_id)
-      VALUES ($1, $2)
-      RETURNING text;
+      INSERT INTO question_option (
+                  text, 
+                  question_id
+      )
+      VALUES (
+                  $1, 
+                  $2
+      )
+      RETURNING   unique_id AS "uniqueId",
+                  id,
+                  text,
+                  question_id
     `;
     const result = await dbClient.query(query, [option, questionId]);
 
@@ -245,15 +326,28 @@ async function addOption(questionId, option) {
  * Stores a given question of a questionnaire in the database.
  */
 async function addQuestion(questionnaireId, question) {
-  const { id, text, options } = question;
+  const { id, text, type, options } = question;
 
   try {
     const query = `
-      INSERT INTO question (id, text, questionnaire_id)
-      VALUES ($1, $2, $3)
-      RETURNING unique_id AS "uniqueId", id, text;
+      INSERT INTO question (
+                  id, 
+                  text, 
+                  type, 
+                  questionnaire_id
+      )
+      VALUES (
+                  $1, 
+                  $2, 
+                  $3, 
+                  $4
+      )
+      RETURNING   unique_id AS "uniqueId", 
+                  id, 
+                  text, 
+                  type
     `;
-    const result = await dbClient.query(query, [id, text, questionnaireId]);
+    const result = await dbClient.query(query, [id, text, type, questionnaireId]);
     const inserted = result.rows[0];
 
     // Insert options for single and multi-select questions
@@ -303,7 +397,63 @@ async function addQuestionnaire(questionnaire) {
       }
     }
 
+    // console.log(inserted);
+
     return inserted;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+/**
+ * Updates a given option of a question stored in the database.
+ */
+async function updateOption(questionId, content) {
+  try {
+    const deletionQuery = `
+      DELETE FROM question_option
+      WHERE       question_id = $1
+    `;
+    await dbClient.query(deletionQuery, [questionId]);
+
+    const updated = await addOption(questionId, content);
+
+    return updated;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+/**
+ * Updates a given question of a questionnaire stored in the database.
+ */
+async function updateQuestion(uniqueId, content) {
+  const { id, text, type, options } = content;
+
+  try {
+    const query = `
+      UPDATE    question
+      SET       id = COALESCE($1, id),
+                text = COALESCE($2, text),
+                type = COALESCE($3, type)
+      WHERE     unique_id = $4
+      RETURNING unique_id AS "uniqueId",
+                id,
+                text
+    `;
+    const result = await dbClient.query(query, [id, text, type, uniqueId]);
+    const updated = result.rows[0];
+
+    if (filled(options)) {
+      updated.options = [];
+
+      for (const incoming of options) {
+        // console.log(incoming);
+        // updated.options.push(await addOption(updated.uniqueId, incoming));
+      }
+    }
+
+    return updated;
   } catch (err) {
     console.error(err);
   }
@@ -312,39 +462,150 @@ async function addQuestionnaire(questionnaire) {
 /**
  * Updates a given questionnaire stored in the database.
  */
-async function updateQuestionnaire(id, content) {
+async function updateQuestionnaire(uniqueId, content) {
   const { name, scored, path, questions } = content;
 
   try {
     const query = `
-      WITH updated AS (
-        UPDATE    questionnaire
-      UPDATE    questionnaire 
-        UPDATE    questionnaire
-        SET       name = COALESCE($1, name),
-                  scored = COALESCE($2, scored), 
-                  file_path = COALESCE($3, file_path) 
-        WHERE     unique_id = $4
-        RETURNING name,
-                  scored,
-                  file_path AS path,
-                  unique_id
-      )
-      SELECT  updated.*,
-              JSON_AGG(JSON_BUILD_OBJECT('uniqueId', question.unique_id, 'id', question.id)) AS questions
-      FROM    updated
-      JOIN    question 
-        ON    question.questionnaire_id = updated.unique_id
-      GROUP BY  updated.unique_id,
-                updated.name,
-                updated.scored,
-                updated.path
-    `;
+      UPDATE    questionnaire
+      SET       name = COALESCE($1, name),
+                scored = COALESCE($2, scored), 
+                file_path = COALESCE($3, file_path) 
+      WHERE     unique_id = $4
+      RETURNING name,
+                scored,
+                file_path AS path,
+                unique_id AS "uniqueId"
+      `;
 
-    const result = await dbClient.query(query, [name, scored, path, id]);
+    const result = await dbClient.query(query, [name, scored, path, uniqueId]);
     const updated = result.rows[0];
 
+    if (filled(questions)) {
+      updated.questions = [];
+
+      // Check new questions against existing questions
+      const existing = await getQuestionnaire(uniqueId);
+      const { questionsSame, questionsDiffs } = compareQuestions(existing.questions, content.questions);
+
+      // console.log(existing.questions);
+
+      if (!questionsSame) console.log(questionsDiffs);
+
+      /* if (!questionsSame && !filled(questionsDiffs)) {
+        // Some questions do not exist in the questionnaire yet
+        console.log(JSON.stringify(questionsDiffs));
+      } */
+
+      /* for (const incoming of questions) {
+        const questionPK = await getQuestionPK(incoming.id, updated.uniqueId);
+ 
+        updated.questions.push(await updateQuestion(questionPK, incoming));
+      } */
+    }
+
     return updated;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getQuestionnaire(uniqueId) {
+  try {
+    const query = `
+      SELECT  questionnaire.unique_id AS "uniqueId",
+              questionnaire.name,
+              questionnaire.scored,
+              questionnaire.file_path AS path,
+              COALESCE(
+                JSON_AGG(questions.*) 
+                FILTER (
+                  WHERE questions."uniqueId" IS NOT NULL
+                ),
+              '[]'
+              ) AS questions
+      FROM (
+        SELECT  question.unique_id AS "uniqueId",
+                question.id,
+                question.text,
+                question.type,
+                question.questionnaire_id,
+                COALESCE(
+                  JSON_AGG(
+                    JSON_BUILD_OBJECT(
+                      'id', question_option.id,
+                      'text', question_option.text
+                    )
+                  ) FILTER (
+                      WHERE question_option.unique_id IS NOT NULL
+                  ),
+                '[]'
+                ) AS options
+        FROM question
+        LEFT JOIN question_option ON question.unique_id = question_option.question_id
+        GROUP BY question.unique_id
+      ) AS questions
+      RIGHT JOIN questionnaire ON questions.questionnaire_id = questionnaire.unique_id
+      WHERE questionnaire.unique_id = $1
+      GROUP BY questionnaire.unique_id
+    `;
+
+    const result = await dbClient.query(query, [uniqueId]);
+    const selected = result.rows[0];
+
+    return selected;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getQuestion(uniqueId) {
+  try {
+    const query = `
+      SELECT    question.unique_id AS "uniqueId",
+                question.id,
+                question.text,
+                question.type,
+                COALESCE(
+                  JSON_AGG(
+                    JSON_BUILD_OBJECT(
+                      'id', question_option.id,
+                      'text', question_option.text
+                    )
+                  ) FILTER (
+                      WHERE question_option.unique_id IS NOT NULL
+                    ),
+                '[]'
+                ) AS options
+      FROM      question
+      LEFT JOIN question_option
+      ON        question_option.question_id = question.unique_id
+      WHERE     question.unique_id = $1
+      GROUP BY  question.unique_id
+    `;
+    const result = await dbClient.query(query, [uniqueId]);
+
+    return result.rows[0];
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+/**
+ * Retrieves the unique identifer for a given question, found using its
+ * human-readable ID and questionnaire ID.
+ */
+async function getQuestionPK(id, questionnaireId) {
+  try {
+    const query = `
+      SELECT  unique_id AS "uniqueId"
+      FROM    question
+      WHERE   id = $1 AND
+              questionnaire_id = $2
+    `;
+    const result = await dbClient.query(query, [id, questionnaireId]);
+
+    return result.rows[0].uniqueId;
   } catch (err) {
     console.error(err);
   }
