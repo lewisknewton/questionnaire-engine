1e33ccfe973d44f60f10a3b0d4505487a25d8579
Lewis Newton <lewisknewton@hotmail.com>
2020-05-21 18:33:36 +0100

    revert to previous schema
    - does not sync files and database records

--- database/schema.sql
+++ database/schema.sql
@@ -4,47 +4,40 @@ CREATE DATABASE questionnaire_engine;
 \c questionnaire_engine;
 
 DROP TABLE IF EXISTS response;
+DROP TABLE IF EXISTS answer;
 DROP TABLE IF EXISTS question;
 DROP TABLE IF EXISTS questionnaire;
-DROP TABLE IF EXISTS question_option;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS questionnaire (
   unique_id uuid DEFAULT uuid_generate_v4(),
   id VARCHAR(8),
-  name VARCHAR(255) NOT NULL,
-  scored BOOLEAN DEFAULT FALSE,
-  file_path TEXT,
+  file_path TEXT UNIQUE NOT NULL,
   author_email VARCHAR(255),
   PRIMARY KEY (unique_id)
 );
 
 CREATE TABLE IF NOT EXISTS question (
   unique_id uuid DEFAULT uuid_generate_v4(),
-  id VARCHAR(30) NOT NULL,
-  text TEXT,
+  content TEXT NOT NULL,
   type VARCHAR(70),
   questionnaire_id uuid NOT NULL,
   PRIMARY KEY (unique_id),
   FOREIGN KEY (questionnaire_id) REFERENCES questionnaire (unique_id)
 );
 
-CREATE TABLE IF NOT EXISTS question_option (
-  unique_id uuid DEFAULT uuid_generate_v4(),
-  id VARCHAR(70),
-  text TEXT,
-  question_id uuid NOT NULL,
-  PRIMARY KEY (unique_id),
-  FOREIGN KEY (question_id) REFERENCES question (unique_id)
-);
-
 CREATE TABLE IF NOT EXISTS response (
   unique_id uuid DEFAULT uuid_generate_v4(),
   id VARCHAR(8),
   time_submitted TIMESTAMP DEFAULT NOW(),
-  questionnaire_id uuid NOT NULL,
-  data JSONB,
+  PRIMARY KEY (unique_id)
+);
+
+CREATE TABLE IF NOT EXISTS answer (
+  unique_id uuid DEFAULT uuid_generate_v4(),
+  content TEXT,
+  response_id uuid NOT NULL,
   PRIMARY KEY (unique_id),
-  FOREIGN KEY (questionnaire_id) REFERENCES questionnaire (unique_id)
+  FOREIGN KEY (response_id) REFERENCES response (unique_id)
 );
