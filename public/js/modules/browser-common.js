e564c2cdc2dc9086c8f8428daa9e8f8eeb9b4886
Lewis Newton <lewisknewton@hotmail.com>
2020-05-31 15:33:35 +0100

    format dates properly
    - uses date (01-31) instead of day (0-6)
    - prepends 0 to hours, mins, and secs under 10

--- public/js/modules/browser-common.js
+++ public/js/modules/browser-common.js
@@ -4,13 +4,13 @@
  * Converts a given Date object into a human-readable format.
  */
 export function getFormattedDate(dateObj) {
-  const day = dateObj.getDay() < 10 ? `0${dateObj.getDay()}` : dateObj.getDay();
+  const day = dateObj.getDate() < 10 ? `0${dateObj.getDate()}` : dateObj.getDate();
   const month = dateObj.getMonth() + 1 < 10 ? `0${dateObj.getMonth() + 1}` : dateObj.getMonth() + 1;
   const year = dateObj.getFullYear();
 
-  const hours = dateObj.getHours();
-  const mins = dateObj.getMinutes();
-  const secs = dateObj.getSeconds();
+  const hours = dateObj.getHours() < 10 ? `0${dateObj.getHours()}` : dateObj.getHours();
+  const mins = dateObj.getMinutes() < 10 ? `0${dateObj.getMinutes()}` : dateObj.getMinutes();
+  const secs = dateObj.getSeconds() < 10 ? `0${dateObj.getSeconds()}` : dateObj.getSeconds();
 
   return `${year}-${month}-${day} ${hours}:${mins}:${secs}`;
 }
