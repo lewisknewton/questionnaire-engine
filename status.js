3e1362845d5b9f0357240283c850be772401c2ad
Lewis Newton <lewisknewton@hotmail.com>
2020-06-02 14:58:59 +0100

    make error messages more descriptive
    - also removes duplicate for when responses are not found

--- status.js
+++ status.js
@@ -14,17 +14,15 @@ const errors = {
   questionnaireNoQuestions:
     'Sorry, no questions have been added to this questionnaire yet. Please check back later.',
   questionnaireNotFound:
-    id => `Sorry, no questionnaire of ID '${id}' could be found.`,
+    id => `Sorry, no questionnaire of ID '${id}' could be found. Please check the ID and try again.`,
   questionnaireNotSelected:
-    'Sorry, no questionnaire was selected. Please try again.',
+    'Sorry, no questionnaire was selected. Please try again using the ID of the questionnaire.',
   questionnairesNotFound:
-    'Sorry, no questionnaires were found.',
+    'Sorry, no questionnaires were found. Please add them to the questionnaires folder.',
   responseNoAnswers:
-    'Sorry, no answers have been provided. Please re-attempt the questionnaire and try again.',
+    'Sorry, no answers have been provided. Please re-attempt the questionnaire and try submitting again.',
   responseNotSaved:
-    'Sorry, your response could not be saved at this time. Please try again.',
-  responsesNotFound:
-    id => `Sorry, no responses for questionnaire of ID '${id}' could be found.`,
+    'Sorry, your response could not be saved at this time. Please try submitting again.',
 };
 
 // Custom warning messages
@@ -32,7 +30,7 @@ const warnings = {
   questionnaireNoQuestionsCreator:
     'No questions have been added to this questionnaire yet. Please add at least one question for your participants to answer.',
   responsesNotFound:
-    id => `Sorry, no responses for questionnaire of ID '${id}' have been given yet.`,
+    id => `Sorry, no responses for questionnaire of ID '${id}' have been given yet. Please check back later.`,
 };
 
 module.exports = {
