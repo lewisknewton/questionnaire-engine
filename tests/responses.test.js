fe4bdaec20f76eb87cb83899e35f9b45e56871b6
Lewis Newton <lewisknewton@hotmail.com>
2020-06-05 18:48:55 +0100

    :art: separate test cases

--- tests/responses.test.js
+++ tests/responses.test.js
@@ -1,10 +1,8 @@
 /* global afterAll, beforeEach, describe, it, expect */
 
 const { generateShortId } = require('../questionnaire-handler');
-const { isFilled } = require('../common');
 const supertest = require('supertest');
 const { codes, errors, warnings } = require('../status');
-const dbClient = require('../database/db-client');
 let server, request;
 
 beforeEach(() => {
@@ -17,97 +15,6 @@ afterAll(async done => {
 });
 
 describe('GET Endpoints', () => {
-  it('should retrieve all questionnaires', async done => {
-    const res = await request.get('/api/questionnaires');
-
-    if (Array.isArray(res.body) && isFilled(res.body)) {
-      // Questionnaires exist in the `questionnaires` directory
-      expect(res.statusCode).toStrictEqual(codes.ok);
-      expect(res.body).toEqual(expect.arrayContaining([
-        expect.objectContaining({
-          id: expect.any(String),
-          name: expect.any(String),
-          questions: expect.arrayContaining([
-            expect.objectContaining({
-              id: expect.any(String),
-              text: expect.any(String),
-              type: expect.any(String),
-              options: expect.arrayContaining([
-                expect.any(String),
-              ]),
-            }),
-          ]),
-        }),
-      ]));
-    } else {
-      // Questionnaires do not exist in the `questionnaires` directory
-      const expected = { error: errors.questionnairesNotFound };
-
-      expect(res.statusCode).toStrictEqual(codes.notFound);
-      expect(res.body).toMatchObject(expected);
-    }
-
-    done();
-  });
-
-  it('should retrieve existent questionnaires by ID', async done => {
-    const testQuestionnaires = await request.get('/api/questionnaires');
-
-    for (const q of testQuestionnaires.body) {
-      const res = await request.get(`/api/questionnaires/${q.id}`);
-      const questions = res.body.questions;
-
-      if (isFilled(questions)) {
-        expect(res.statusCode).toStrictEqual(codes.ok);
-        expect(res.body).toMatchObject(q);
-      }
-    }
-
-    done();
-  });
-
-  it('should not retrieve non-existent questionnaires by ID', async done => {
-    const nonExistent = 'does-not-exist-123';
-    const expected = { error: errors.questionnaireNotFound(nonExistent) };
-
-    const res = await request.get(`/api/questionnaires/${nonExistent}`);
-
-    expect(res.statusCode).toStrictEqual(codes.notFound);
-    expect(res.body).toMatchObject(expected);
-
-    done();
-  });
-
-  it('should not retrieve empty questionnaire IDs', async done => {
-    const nonExistent = null;
-    const expected = { error: errors.questionnaireNotSelected };
-
-    const res = await request.get(`/api/questionnaires/${nonExistent}`);
-
-    expect(res.statusCode).toStrictEqual(codes.badRequest);
-    expect(res.body).toMatchObject(expected);
-
-    done();
-  });
-
-  it('should not display questionnaires with no questions', async done => {
-    const testQuestionnaires = await request.get('/api/questionnaires');
-
-    for (const q of testQuestionnaires.body) {
-      const res = await request.get(`/api/questionnaires/${q.id}`);
-      const questions = res.body.questions;
-
-      const expected = { ...res.body, error: errors.questionnaireNoQuestions };
-
-      if (!isFilled(questions)) {
-        expect(res.statusCode).toStrictEqual(codes.notFound);
-        expect(res.body).toMatchObject(expected);
-      }
-    }
-
-    done();
-  });
-
   it('should retrieve all responses for a given questionnaire', async done => {
     const testQuestionnaires = await request.get('/api/questionnaires');
 
